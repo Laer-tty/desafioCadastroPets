81c3e3cb77464639bf23c6051b5a5591e0c9dd0d
Laer-tty <emaildolaertty@gmail.com>
2026-04-11 18:08:40 -0300

    Adicionando projeto springboot

---
+++ backend/src/main/java/com/github/laertty/sistema_cadastro_pets/SistemaCadastroPetsApplication.java
@@ -0,0 +1,13 @@
+package com.github.laertty.sistema_cadastro_pets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCadastroPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCadastroPetsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/laertty/sistema_cadastro_pets/SistemaCadastroPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.laertty.sistema_cadastro_pets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCadastroPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
